6ce45a1a2df8d7c7e2c5f0fd08d0f37ebc1cd0b5
Jamie <jamielynjenks@gmail.com>
2021-02-15 17:32:13 -0700

    finished services and service implementations

--- src/main/java/com/lambdaschool/orders/services/AgentServiceImpl.java
+++ src/main/java/com/lambdaschool/orders/services/AgentServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service(value = "agentServices")
 public class AgentServiceImpl implements AgentService{
     @Autowired
--- src/main/java/com/lambdaschool/orders/services/OrdersServiceImpl.java
+++ src/main/java/com/lambdaschool/orders/services/OrdersServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 public class OrdersServiceImpl implements OrdersService {
     @Autowired
