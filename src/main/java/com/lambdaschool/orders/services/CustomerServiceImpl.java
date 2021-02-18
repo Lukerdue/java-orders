aaa8ece68decbcbfba4b4fb195d32c83f47df967
Jamie <jamielynjenks@gmail.com>
2021-02-17 17:56:27 -0700

    Can post new customer with all its relationships

--- src/main/java/com/lambdaschool/orders/services/CustomerServiceImpl.java
+++ src/main/java/com/lambdaschool/orders/services/CustomerServiceImpl.java
@@ -1,7 +1,12 @@
 package com.lambdaschool.orders.services;
 
+import com.lambdaschool.orders.models.Agent;
 import com.lambdaschool.orders.models.Customer;
+import com.lambdaschool.orders.models.Order;
+import com.lambdaschool.orders.models.Payment;
+import com.lambdaschool.orders.repositories.AgentsRepository;
 import com.lambdaschool.orders.repositories.CustomersRepository;
+import com.lambdaschool.orders.repositories.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +20,56 @@ import java.util.List;
 public class CustomerServiceImpl implements CustomerService {
     @Autowired
     CustomersRepository custrepo;
+    @Autowired
+    PaymentRepository payrepo;
+    @Autowired
+    AgentsRepository agentrepo;
 
     @Transactional
     @Override
-    public Customer save(Customer customer) {
-        return custrepo.save(customer);
+    public Customer save(Customer tempcustomer) {
+        Customer newcust = new Customer();
+
+        //put or post?
+        if(tempcustomer.getCustcode() != 0){
+            custrepo.findById(tempcustomer.getCustcode())
+                    .orElseThrow(()-> new EntityNotFoundException("Customer with id: "+ tempcustomer.getCustcode()+" was not found"));
+            newcust.setCustcode(tempcustomer.getCustcode());
+        }
+
+        newcust.setReceiveamt(tempcustomer.getReceiveamt());
+        newcust.setPhone(tempcustomer.getPhone());
+        newcust.setOutstandingamt(tempcustomer.getOutstandingamt());
+        newcust.setOpeningamt(tempcustomer.getOpeningamt());
+        newcust.setGrade(tempcustomer.getGrade());
+        newcust.setCustcountry(tempcustomer.getCustcountry());
+        newcust.setCustcity(tempcustomer.getCustcity());
+        newcust.setCustname(tempcustomer.getCustname());
+        newcust.setWorkingarea(tempcustomer.getWorkingarea());
+        newcust.setPaymentamt(tempcustomer.getPaymentamt());
+
+        for(Order o: tempcustomer.getOrders()){
+            Order temporder = new Order();
+            temporder.setOrderdescription(o.getOrderdescription());
+            temporder.setOrdamount(o.getOrdamount());
+            temporder.setAdvanceamount(o.getAdvanceamount());
+
+            for(Payment p: o.getPayments()){
+                Payment pay = payrepo.findById(p.getPaymentid())
+                        .orElseThrow(()->new EntityNotFoundException("payment with id: "+p.getPaymentid()+"not found"));
+                temporder.getPayments().add(pay);
+            }
+            temporder.setCustomer(newcust);
+            newcust.getOrders().add(temporder);
+        }
+        if(agentrepo.findById(tempcustomer.getAgent().getAgentcode()).isPresent()){
+            Agent agent = agentrepo.findById(tempcustomer.getAgent().getAgentcode())
+                    .orElseThrow(()->new EntityNotFoundException("agent "+tempcustomer.getAgent().getAgentcode()+" was not found and could not be added"));
+            newcust.setAgent(agent);
+        }
+
+
+        return custrepo.save(newcust);
     }
 
     @Override
@@ -40,5 +90,47 @@ public class CustomerServiceImpl implements CustomerService {
     public List<Customer> custNameLike(String name) {
         List<Customer> customers = custrepo.findCustomersByCustnameContainingIgnoreCase(name);
         return customers;
+    }
+
+    @Override
+    public void deleteCustomer(long custcode) {
+        custrepo.deleteById(custcode);
+    }
+
+    @Override
+    public Customer updateCustomer(long custcode, Customer customer) {
+        Customer updateCust = custrepo.findById(custcode)
+                .orElseThrow(()-> new EntityNotFoundException("Customer "+custcode+" not found"));
+
+
+        if(customer.hasvaluereceiveamt) {updateCust.setReceiveamt(customer.getReceiveamt());}
+        if(customer.getPhone() != null) {updateCust.setPhone(customer.getPhone());}
+        if(customer.hasvalueoutstandingamt) {updateCust.setOutstandingamt(customer.getOutstandingamt());}
+        if(customer.hasvalueopeningamt){updateCust.setOpeningamt(customer.getOpeningamt());}
+        if(customer.getGrade() != null) {updateCust.setGrade(customer.getGrade());}
+        if(customer.getCustcountry() != null) {updateCust.setCustcountry(customer.getCustcountry());}
+        if(customer.getCustcity() != null){updateCust.setCustcity(customer.getCustcity());}
+        if(customer.getCustname() != null){updateCust.setCustname(customer.getCustname());}
+        if(customer.getAgent() != null ){updateCust.setAgent(customer.getAgent());}
+        if(customer.getWorkingarea() != null){updateCust.setWorkingarea(customer.getWorkingarea());}
+        if(customer.hasvaluepaymentamt){updateCust.setPaymentamt(customer.getPaymentamt());}
+
+        if(customer.getOrders().size() > 0) {
+            updateCust.getOrders().clear();
+            for (Order o : customer.getOrders()) {
+                Order temporder = new Order();
+                temporder.setCustomer(updateCust);
+                temporder.setOrderdescription(o.getOrderdescription());
+                temporder.setOrdamount(o.getOrdamount());
+                temporder.setAdvanceamount(o.getAdvanceamount());
+
+                for (Payment p : o.getPayments()) {
+                    Payment pay = payrepo.findById(p.getPaymentid())
+                            .orElseThrow(() -> new EntityNotFoundException("payment with id: " + p.getPaymentid() + "not found"));
+                    o.getPayments().add(pay);
+                }
+            }
+        }
+        return updateCust;
     }
 }
--- src/main/java/com/lambdaschool/orders/services/OrdersServiceImpl.java
+++ src/main/java/com/lambdaschool/orders/services/OrdersServiceImpl.java
@@ -1,7 +1,9 @@
 package com.lambdaschool.orders.services;
 
 import com.lambdaschool.orders.models.Order;
+import com.lambdaschool.orders.models.Payment;
 import com.lambdaschool.orders.repositories.OrdersRepository;
+import com.lambdaschool.orders.repositories.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +17,31 @@ import java.util.List;
 public class OrdersServiceImpl implements OrdersService {
     @Autowired
     private OrdersRepository orderrepo;
+    @Autowired
+    private PaymentRepository payrepo;
 
     @Transactional
     @Override
-    public Order save(Order order) {
-        return orderrepo.save(order);
+    public Order save(Order temporder) {
+        Order neworder = new Order();
+        if(temporder.getOrdnum() != 0){
+            orderrepo.findById(temporder.getOrdnum())
+                    .orElseThrow(()->new EntityNotFoundException("order number "+temporder.getOrdnum() + "could not be found"));
+            neworder.setOrdnum(temporder.getOrdnum());
+        }
+
+        neworder.setCustomer(temporder.getCustomer());
+        neworder.setOrderdescription(temporder.getOrderdescription());
+        neworder.setOrdamount(temporder.getOrdamount());
+        neworder.setAdvanceamount(temporder.getAdvanceamount());
+
+        for(Payment pay:temporder.getPayments()){
+            Payment p = payrepo.findById(pay.getPaymentid())
+                    .orElseThrow(()->new EntityNotFoundException("payment with id: "+pay.getPaymentid()+" could not be found"));
+            neworder.getPayments().add(p);
+        }
+
+        return orderrepo.save(neworder);
     }
 
     @Override
@@ -34,5 +56,15 @@ public class OrdersServiceImpl implements OrdersService {
         Order order = orderrepo.findById(id)
                 .orElseThrow(()->new EntityNotFoundException("Order with number: "+id+" not found"));
         return order;
+    }
+
+    @Override
+    public void deleteById(long id) {
+        if(orderrepo.findById(id).isPresent()){
+            orderrepo.deleteById(id);
+        }
+        else{
+            throw new EntityNotFoundException("order with number: "+id+"could not be found");
+        }
     }
 }
